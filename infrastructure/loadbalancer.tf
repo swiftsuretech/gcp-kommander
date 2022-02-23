52780d19b2015e8b594f554468dabdebf3b664ce
Dave Whitehouse <dave@davewhitehouse.co.uk>
2022-02-23 23:33:49 +0000

    added tgt gps

--- infrastructure/loadbalancer.tf
+++ infrastructure/loadbalancer.tf
@@ -3,11 +3,11 @@ resource "google_compute_region_backend_service" "pega-backend-service" {
   connection_draining_timeout_sec = 300
   enable_cdn                      = false
   health_checks = [
-    "google_compute_instance_group.pega-instance-group.id"
+    google_compute_target_pool.pega-target-pool.id
   ]
   load_balancing_scheme = "INTERNAL"
   name                  = "pega-load-balancer"
-  network               = "google_compute_network.pega-network.id"
+  network               = google_compute_network.pega-network.id
   project               = "konvoy-gcp-se"
   protocol              = "TCP"
   region                = var.region
@@ -18,7 +18,7 @@ resource "google_compute_region_backend_service" "pega-backend-service" {
     balancing_mode               = "CONNECTION"
     capacity_scaler              = 0
     failover                     = false
-    group                        = "https://www.googleapis.com/compute/v1/projects/konvoy-gcp-se/zones/us-central1-a/instanceGroups/pega-instance-group"
+    group                        = google_compute_target_pool.pega-target-pool.id
     max_connections              = 0
     max_connections_per_endpoint = 0
     max_connections_per_instance = 0
@@ -29,23 +29,16 @@ resource "google_compute_region_backend_service" "pega-backend-service" {
   }
 }
 
-resource "google_compute_instance_group" "pega-instance-group" {
-  instances = [
-    "google_compute_instance.pega-control-plane0.id",
-  ]
-  name    = "pega-instance-group"
-  network = google_compute_network.pega-network.id
-  project = var.project
-  zone    = "us-central1-a"
+resource "google_compute_target_pool" "pega-target-pool" {
+  instances     = google_compute_instance.pega-control-plane.*.self_link
+  name          = "pega-target-pool"
+  project       = var.project
+  health_checks = [google_compute_http_health_check.pega-http-health-check.self_link]
 }
 
-resource "google_compute_health_check" "pega-health-check" {
-  name = "pega-health-check"
-
+resource "google_compute_http_health_check" "pega-http-health-check" {
+  name               = "pega-http-health-check"
+  request_path       = "/"
   timeout_sec        = 5
   check_interval_sec = 10
-
-  tcp_health_check {
-    port = "80"
-  }
 }
