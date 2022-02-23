7fa0cd6b88691dc419a41548b67fe4ff259a175d
swiftsuretech <dave@davewhitehouse.co.uk>
2022-02-23 16:52:46 +0000

    tidy up

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "pega" {
+resource "google_compute_network" "pega-network" {
   auto_create_subnetworks         = true
   delete_default_routes_on_create = false
   mtu                             = 0
@@ -7,10 +7,10 @@ resource "google_compute_network" "pega" {
   routing_mode                    = "REGIONAL"
 }
 
-resource "google_compute_subnetwork" "pega" {
+resource "google_compute_subnetwork" "pega-subnet" {
   ip_cidr_range              = "10.0.0.0/16"
   name                       = "pega-subnet"
-  network                    = "https://www.googleapis.com/compute/v1/projects/konvoy-gcp-se/global/networks/pega-network"
+  network                    = "google_compute_network.pega-network.id"
   private_ip_google_access   = false
   private_ipv6_google_access = "DISABLE_GOOGLE_ACCESS"
   project                    = "konvoy-gcp-se"
@@ -18,39 +18,12 @@ resource "google_compute_subnetwork" "pega" {
   region                     = "us-central1"
   secondary_ip_range         = []
   stack_type                 = "IPV4_ONLY"
-  depends_on                 = [google_compute_network.pega]
+  depends_on                 = [google_compute_network.pega-network]
 }
-
-/* resource "google_compute_firewall" "pega" {
-  direction  = "INGRESS"
-  depends_on = [google_compute_network.pega]
-  disabled   = false
-  name       = "pega-ssh-access"
-  network    = "https://www.googleapis.com/compute/v1/projects/konvoy-gcp-se/global/networks/pega-network"
-  priority   = 1000
-  project    = "konvoy-gcp-se"
-  source_ranges = [
-    "0.0.0.0/0",
-  ]
-  target_service_accounts = [
-    "dswhitehouse@konvoy-gcp-se.iam.gserviceaccount.com",
-  ]
-
-  allow {
-    ports = [
-      "22",
-    ]
-    protocol = "tcp"
-  }
-  allow {
-    ports    = []
-    protocol = "icmp"
-  }
-} */
 
 resource "google_compute_firewall" "pega-internal" {
   direction  = "INGRESS"
-  depends_on = [google_compute_network.pega]
+  depends_on = [google_compute_network.pega-network]
   disabled   = false
   name       = "pega-internal"
   network    = "https://www.googleapis.com/compute/v1/projects/konvoy-gcp-se/global/networks/pega-network"
