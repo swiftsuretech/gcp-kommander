204e1eb2e1ad9c364583fe2a57ce7f3405bf0616
swiftsuretech <dave@davewhitehouse.co.uk>
2022-02-23 16:53:05 +0000

    add owner and public key var

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -43,5 +43,11 @@ variable "cluster_name" {
 }
 
 variable "public_key" {
-  type = string
+  type    = string
+  default = "dswhitehouse:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDV5Qx5+2IDK1UvSqj4MOGLFQPABrWNQeWGHHtblO4bfb0SBRTJ4aBSxTGTBZzVUK14nvgPT2M/oogZdEytxhLTRSMKX0LraMww0BnRM1/rDtvfzk9qCRy7CH6H2r2N9Yh0jrvp18ZwwkvBvhl3i3P8cIDd1/y5VLq//4TWnHLf/PE/tDFOqESUYoYJglynKHZaQkPIj2Sh/0XIdRRWTrbSVt9wKAs2J9oNB57c4l8Ri2syZp1mZP7s4JQLqDSiYs4ghk9YrGqMW1Ps1KmNApWjkjfIPEPPkA3WwYg/x5hmSo+hwDMP2/8/IL8L2asLF8GP9zMQoVAga+k5tJ+umzxDkfGfwa16UphG0AbY+76zKy5O1BYGrYWE3pzmrZSSe+ukeilWowyjrdR4m/aAwI+5uubiw4b/zZ5Sbgx2P0SX9AektGWIkRSg0RPBcwUOCrrkpbn7Lo1+NKc5jzwP5Yzohs5b2PpaIDZ2vBP+i9ZFZzV4B+B6SPeKlHBns3tGQyc= dswhitehouse@DW-WIN-DESKTOP"
+}
+
+variable "owner" {
+  type    = string
+  default = "davewhitehouse"
 }
