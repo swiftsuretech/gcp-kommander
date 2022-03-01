0d5eae4da9fda4b1af571719de1bed839ae79248
swiftsuretech <dave@davewhitehouse.co.uk>
2022-03-01 15:34:35 +0000

    removed untar bits

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,3 +3,12 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
+
+locals {
+  bastion_l = google_compute_instance.pega-bastion.network_interface.0.access_config.0.nat_ip
+  connect   = "ssh -i keys/pega $local.bastion_1"
+}
+
+output "output" {
+  value = local.connect
+}
